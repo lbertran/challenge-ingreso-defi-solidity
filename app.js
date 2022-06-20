9e8e8d8173718ce0d17ef0c3ee2e2ee546169f66
Luciano Bertran <bertranluciano@gmail.com>
2022-06-20 20:56:21 -0300

    begin

--- app.js
+++ app.js
@@ -3,98 +3,132 @@
 
 // https://www.youtube.com/watch?v=zsnHIlsUPSU
 import express from "express";
-import dateFormat, { masks } from "dateformat";
 import bodyParser from "body-parser";
-import StellarSdk from 'stellar-sdk';
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 const PORT = process.env.PORT || 3050;
 const app = express();
 app.use(bodyParser.json());
+dotenv.config();
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
 
 // Route
 app.get('/',(req, res) => {
-    res.send('Welcome to BlockchainCore API by TOB GROUP SOLUTIONS')
+    res.send('Challenge Ingreso Career Switch: DeFI-Solidity Edition - author: bertranluciano@gmail.com - repositorio: https://github.com/lbertran/challenge-ingreso-defi-solidity')
 });
 
-const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
-dotenv.config();
+app.get('/check',(req, res) => {
 
-const sourceKeys = StellarSdk.Keypair.fromSecret(process.env.SECRET);
-const destination = "GCL2MRTHUH44RBWPTL6UDHT754UX2QRRGGFXSYFZJLFHRVFQ6UFDNRL4";
+    const email = req.query.email;
 
-
-
-// STELLAR
-
-app.post('/stampHashStellar',(req, res) => {
+    check(email).then(data => console.log(data));
     
-    const memo = new StellarSdk.Memo(
-        "hash",
-        req.body.hash
-    );
-
-    const sendTransaction = async () => {
-        try {
-            // Revisamos que la cuenta exista para evitar errores
-            // y cobros innecesarios de comisiones
-            await server.loadAccount(destination);
-            const sourceAccount = await server.loadAccount(sourceKeys.publicKey());
-
-            // Armamos la transacción
-            const transaction = new StellarSdk.TransactionBuilder(sourceAccount, {
-            fee: StellarSdk.BASE_FEE,
-            networkPassphrase: StellarSdk.Networks.TESTNET
-            })
-            .addOperation(
-                StellarSdk.Operation.payment({
-                destination: destination,
-                // Dado que Stellar permite transacciones en diferentes
-                // tipos de cambio, debes especificar la moneda en la que enviarás
-                // El tipo "native" se refiere a Lumens (XLM)
-                asset: StellarSdk.Asset.native(),
-                // https://developers.stellar.org/docs/issuing-assets/anatomy-of-an-asset/
-                //The smallest non-zero amount unit is 0.0000001 (one ten-millionth) represented as an integer value of one.
-                amount: "0.0000001"
-                })
-            )
-            .addMemo(
-                memo
-            )
-            // Espera un máximo de tres minutos por la transacción
-            .setTimeout(180)
-            .build();
-
-            // Firmamos la transacción para autenticar nuestra identidad
-            transaction.sign(sourceKeys);
-            // Finalmente la enviamos a Stellar
-            const result = await server.submitTransaction(transaction);
-            console.log("Success! Results:", result);
-            
-            res.json(result);
-        } catch (err) {
-            console.error("An error has occurred", err);
-            res.send("An error has occurred", err);
-        }
-    };
-    sendTransaction();
+    
+    
 });
 
-app.post('/validateHashStellar',(req, res) => {
-    server
-    .transactions()
-    .transaction(
-        req.body.transaction,
-    )
-    .call()
-    .then(function (resp) {
-        console.log(resp);
-        res.json(resp);
-    })
-    .catch(function (err) {
+async function check(email){
+    let tokenObject = await getToken(email)
+    
+    let blocksObject = await getBlocks(tokenObject.token)
+
+    let stringfinal = await ordenamiento(tokenObject.token, blocksObject.data)
+
+    return stringfinal
+    
+}
+
+async function getToken(email) {
+    try {
+       let res = await axios({
+            url: process.env.RCSAPI+'token?email='+email,
+            method: 'get',
+            timeout: 8000,
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        if(res.status == 200){
+            // test for status you want, etc
+            console.log(res.status)
+        }    
+        // Don't forget to return something   
+        return res.data
+    }
+    catch (err) {
         console.error(err);
-        res.send("An error has occurred", err);
-    });
-});
+    }
+}
+
+async function getBlocks(token) {
+    try {
+       let res = await axios({
+            url: process.env.RCSAPI+'blocks?token='+token,
+            method: 'get',
+            timeout: 8000,
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        if(res.status == 200){
+            // test for status you want, etc
+            console.log(res.status)
+        }    
+        // Don't forget to return something   
+        return res.data
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+async function ordenamiento(token, blocks) {
+    try {
+        const cantidadbloques = blocks.length
+        const arrayordenado = []
+        arrayordenado[0] = blocks[0]
+        blocks.splice(0, 1);
+
+        var pocisionbuscada = 1;
+
+        while(pocisionbuscada <= cantidadbloques){
+            var chequeo = false;
+
+            blocks.forEach(element => {
+                if(!chequeo){
+                    let res = axios({
+                        url: process.env.RCSAPI+'check?token='+token,
+                        method: 'post',
+                        timeout: 8000,
+                        headers: {},
+                        data: {
+                            blocks: [arrayordenado[pocisionbuscada-1],element]
+                        }
+                    });
+                    if(res.status == 200){
+                        // test for status you want, etc
+                        chequeo = res.data.message
+                        if(chequeo){
+                            arrayordenado[pocisionbuscada] = blocks[indicecorrecto]
+                            blocks.splice(indicecorrecto, 1);
+                            pocisionbuscada = blocks.length + 1 
+                        };
+                    }; 
+                }
+                
+                
+            });
+
+            //
+
+            
+        }
+        // Don't forget to return something   
+        return blocks
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
